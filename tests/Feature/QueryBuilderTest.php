95f9691f2aaf9450d2f1d8724e52af9605744bc1
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2023-06-23 15:08:44 +0700

    Query Builder Raw

--- tests/Feature/QueryBuilderTest.php
+++ tests/Feature/QueryBuilderTest.php
@@ -331,6 +331,23 @@ class QueryBuilderTest extends TestCase
 
     }
 
+    public function testQueryBuilderRaw()
+    {
+        $this->insertProducts();
+
+        $collection = DB::table("products")
+            ->select(
+                DB::raw("count(id) as total_product"),
+                DB::raw("min(price) as min_price"),
+                DB::raw("max(price) as max_price"),
+            )->get();
+
+        self::assertEquals(2, $collection[0]->total_product);
+        self::assertEquals(18000000, $collection[0]->min_price);
+        self::assertEquals(20000000, $collection[0]->max_price);
+
+    }
+
 
 }
 
