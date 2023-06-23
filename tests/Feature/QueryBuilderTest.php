5b9deb60fe1e5e13bf79593923668afc1b816405
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2023-06-23 16:39:44 +0700

    Cursor Pagination

--- tests/Feature/QueryBuilderTest.php
+++ tests/Feature/QueryBuilderTest.php
@@ -453,6 +453,27 @@ class QueryBuilderTest extends TestCase
         }
     }
 
+    public function testCursorPagination()
+    {
+        $this->insertCategories();
+
+        $cursor = "id";
+        while (true) {
+            $paginate = DB::table("categories")->orderBy("id")->cursorPaginate(perPage: 2, cursor: $cursor);
+
+            foreach ($paginate->items() as $item) {
+                self::assertNotNull($item);
+                Log::info(json_encode($item));
+            }
+
+            $cursor = $paginate->nextCursor();
+            if ($cursor == null) {
+                break;
+            }
+        }
+
+    }
+
 
 }
 
