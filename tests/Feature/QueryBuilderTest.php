ebe03bf0ccc5a30e0d7be3ff8db5cbe5b4478b2f
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2023-06-23 14:50:05 +0700

    Chunk Result

--- tests/Feature/QueryBuilderTest.php
+++ tests/Feature/QueryBuilderTest.php
@@ -195,7 +195,8 @@ class QueryBuilderTest extends TestCase
 
     }
 
-    public function insertProducts(){
+    public function insertProducts()
+    {
         $this->insertCategories();
 
         DB::table("products")->insert([
@@ -254,6 +255,33 @@ class QueryBuilderTest extends TestCase
         $collection->each(function ($item) {
             Log::info(json_encode($item));
         });
+    }
+
+    public function insertManyCategories()
+    {
+        for ($i = 0; $i < 100; $i++) {
+            DB::table("categories")->insert([
+                "id" => "CATEGORY-$i",
+                "name" => "Category $i",
+                "created_at" => "2020-10-10 10:10:10"
+            ]);
+        }
+    }
+
+    public function testChunk()
+    {
+        $this->insertManyCategories();
+
+        DB::table("categories")->orderBy("id")
+            ->chunk(10, function ($categories) {
+                self::assertNotNull($categories);
+                Log::info("Start Chunk");
+                $categories->each(function ($category){
+                    Log::info(json_encode($category));
+                });
+                Log::info("End Chunk");
+            });
+
     }
 
 
